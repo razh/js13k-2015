9415f7c097ded865e2d2cf4d98d7499c863baafb
Raymond Zhou <somecrazyidiot@gmail.com>
2015-08-18 23:24:26 -0400

    Add game entry point.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
-console.log( 'Hello world!' );
+'use strict';
+
+var Game = require( './game' );
+var Object3D = require( './object3d' );
+
+var $ = document.querySelector.bind( document );
+
+var WIDTH = 852;
+var HEIGHT = 480;
+
+function append( parent, el ) {
+  parent.appendChild( el );
+}
+
+var game = new Game(
+  Math.min( window.innerWidth,  WIDTH  ),
+  Math.min( window.innerHeight, HEIGHT )
+);
+
+var container = $( '#g' );
+append( container, game.canvas );
+
+var scene;
+
+function reset() {
+  scene = game.scene = new Object3D();
+}
+
+reset();
+game.play();
