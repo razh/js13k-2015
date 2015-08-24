7a88fca1cf98e172b27367614bf9456f05dbbe8e
Raymond Zhou <somecrazyidiot@gmail.com>
2015-08-23 23:42:31 -0400

    Copy plane geometry creation function.

---
+++ src/js/geometry/plane-geometry.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var Face3 = require( './Face3' );
+var Geometry = require( './geometry' );
+var Vector3 = require( './../math/vector3' );
+
+module.exports = function createPlaneGeometry(
+  width,
+  height,
+  widthSegments,
+  heightSegments
+) {
+  var widthHalf = width / 2;
+  var heightHalf = height / 2;
+
+  var gridX = widthSegments || 1;
+  var gridZ = heightSegments || 1;
+
+  var gridX1 = gridX + 1;
+  var gridZ1 = gridZ + 1;
+
+  var segmentWidth = width / gridX;
+  var segmentHeight = height / gridZ;
+
+  var geometry = new Geometry();
+  var vertices = geometry.vertices;
+  var faces = geometry.faces;
+
+  var x, z;
+  var ix, iz;
+  for ( iz = 0; iz < gridZ1; iz++ ) {
+    z = iz * segmentHeight - heightHalf;
+
+    for ( ix = 0; ix < gridX1; ix++ ) {
+      x = ix * segmentWidth - widthHalf;
+      vertices.push( new Vector3( x, 0, -z ) );
+    }
+  }
+
+  var a, b, c, d;
+  for ( iz = 0; iz < gridZ; iz++ ) {
+    for ( ix = 0; ix < gridX; ix++ ) {
+      a = ix + gridX1 * iz;
+      b = ix + gridX1 * ( iz + 1 );
+      c = ( ix + 1 ) + gridX1 * ( iz + 1 );
+      d = ( ix + 1 ) + gridX1 * iz;
+
+      faces.push( new Face3( a, d, b ) );
+      faces.push( new Face3( b, d, c ) );
+    }
+  }
+
+  geometry.computeFaceNormals();
+
+  return geometry;
+};
