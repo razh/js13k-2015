b78f66ddf279a87643a6226cad316619da40f9c8
Raymond Zhou <somecrazyidiot@gmail.com>
2015-08-30 19:24:58 -0400

    Simplify Spring methods.

--- src/js/math/spring.js
+++ src/js/math/spring.js
@@ -2,29 +2,67 @@
 
 var EPSILON = 1e-4;
 
+function update( dt, x, v, to, k, b ) {
+  var a = k * ( to - x ) - b * v;
+
+  var vf = v + a  * dt;
+  var xf = x + vf * dt;
+
+  if ( Math.abs( vf - v ) < EPSILON &&
+       Math.abs( xf - x ) < EPSILON ) {
+    return [ to, 0 ];
+  }
+
+  return [ xf, vf ];
+}
+
+// Spring for shallow objects.
 function Spring( k, b ) {
   this.k = k;
   this.b = b;
 
-  this.x = 0;
-  this.v = 0;
-  this.to = 0;
+  this.state = {};
+  this.to = {};
 }
 
 Spring.prototype.update = function( dt ) {
-  var a = this.k * ( this.to - this.x ) - this.b * this.v;
+  Object.keys( this.state ).map(function( key ) {
+    if ( this.to[ key ] !== undefined ) {
+      this.state[ key ] = update(
+        dt,
+        this.state[ key ][ 0 ],
+        this.state[ key ][ 1 ],
+        this.to[ key ],
+        this.k,
+        this.b
+      );
+    }
+  }, this );
+};
 
-  var v = this.v + a * dt;
-  var x = this.x + v * dt;
+Spring.prototype.set = function( source ) {
+  this.state = Object.keys( source ).reduce(function( object, key ) {
+    var value = source[ key ];
+    if ( isFinite( value ) ) {
+      object[ key ] = [ value, 0 ];
+    }
 
-  if ( Math.abs( v - this.v ) < EPSILON &&
-       Math.abs( x - this.x ) < EPSILON ) {
-    this.x = this.to;
-    this.v = 0;
-  }
+    return object;
+  }, {} );
 
-  this.x = x;
-  this.v = v;
+  return this;
+};
+
+Spring.prototype.getValues = function() {
+  var state = this.state;
+
+  return Object.keys( this.to ).reduce(function( object, key ) {
+    if ( state[ key ] ) {
+      object[ key ] = state[ key ][ 0 ];
+    }
+
+    return object;
+  }, {} );
 };
 
 module.exports = Spring;
