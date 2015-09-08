1cf5366400a0b330b62bbf2b1d8235a53e6f7555
Raymond Zhou <somecrazyidiot@gmail.com>
2015-09-07 23:43:13 -0400

    Add fBm 2D function.

---
+++ src/js/math/fbm.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var noise2d = require( './noise2d' );
+
+var octaves = 8;
+var period = 4;
+var lacunarity = 2;
+var gain = 0.5;
+
+module.exports = function fbm( x, y ) {
+  var frequency = 1 / period;
+  var amplitude = gain;
+
+  var sum = 0;
+  for ( var i = 0; i < octaves; i++ ) {
+    sum += amplitude * noise2d( x * frequency, y * frequency );
+
+    frequency *= lacunarity;
+    amplitude *= gain;
+  }
+
+  return sum;
+};
