cffc28437ed005a4a6dc1b5b530663c2252da9ee
Raymond Zhou <somecrazyidiot@gmail.com>
2015-09-12 13:10:57 -0400

    Add Vector3 and Camera dat-gui debug functions. Print color values to console.

--- src/js/debug.js
+++ src/js/debug.js
@@ -9,6 +9,10 @@ style.left = 0;
 style.right = 'initial';
 style.float = 'left';
 
+function round( value, precision ) {
+  return parseFloat( value.toFixed( precision ) );
+}
+
 exports.color = function( color, name ) {
   name = name || 'color';
 
@@ -23,9 +27,17 @@ exports.color = function( color, name ) {
   gui.addColor( options, name )
     .listen()
     .onChange(function( value ) {
-      color.r = value[0] / 255;
-      color.g = value[1] / 255;
-      color.b = value[2] / 255;
+      color.setRGB(
+        value[0] / 255,
+        value[1] / 255,
+        value[2] / 255
+      );
+
+      console.log(
+        round( color.r, 2 ) + ', ' +
+        round( color.g, 2 ) + ', ' +
+        round( color.b, 2 )
+      );
     });
 };
 
@@ -34,15 +46,30 @@ exports.material = function( material ) {
   exports.color( material.emissive, 'emissive' );
 };
 
+exports.vector3 = function( vector, range ) {
+  var halfRange = 0.5 * range;
+  gui.add( vector, 'x', -halfRange, halfRange );
+  gui.add( vector, 'y', -halfRange, halfRange );
+  gui.add( vector, 'z', -halfRange, halfRange );
+};
+
 exports.light = function( light ) {
   exports.color( light.color, 'color' );
   gui.add( light, 'intensity', 0, 8 );
-  gui.add( light.position, 'x', -8, 8 );
-  gui.add( light.position, 'y', -8, 8 );
-  gui.add( light.position, 'z', -8, 8 );
+  exports.vector3( light.position, 16 );
 };
 
 exports.spring = function( spring ) {
   gui.add( spring, 'k', 10, 640, 10 );
   gui.add( spring, 'b', 1, 48 );
+};
+
+exports.camera = function( camera ) {
+  gui.add( camera, 'fov', 10, 120, 10 )
+    .listen()
+    .onChange(function() {
+      camera.updateProjectionMatrix();
+    });
+
+  exports.vector3( camera.position, 32 );
 };
